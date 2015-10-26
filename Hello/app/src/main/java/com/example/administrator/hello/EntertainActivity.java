5e67fd56fcbb294637eaf9d482b6e6edcf099336
Ashley05 <704349913@qq.com>
2015-10-26 15:24:20 +0800

    jiamin commit

--- Hello/app/src/main/java/com/example/administrator/hello/EntertainActivity.java
+++ Hello/app/src/main/java/com/example/administrator/hello/EntertainActivity.java
@@ -22,12 +22,13 @@ public class EntertainActivity extends Activity {
     ListView mylist;
     //设置图片
     private int[] imageIds=new int[]
-            {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.f,R.drawable.e,R.drawable.aa,R.drawable.bb,
-            R.drawable.easy,R.drawable.entertain};
+            {R.drawable.i1,R.drawable.i2,R.drawable.i3,R.drawable.i4,R.drawable.i5,R.drawable.i6,R.drawable.i7,R.drawable.i8,
+                    R.drawable.i9,R.drawable.i10};
     //设置视频信息
     private String[] names=new String[]
             {
-                    "三只小猪要独立生活","三只小猪要盖房子","小猪盖房子","","","","","","",""
+                    "三只小猪要独立生活","三只小猪要盖房子","大灰狼吹倒两座房子","大灰狼的尾巴着火了","大灰狼的大弟弟鼻子破了",
+                    "大灰狼的小弟弟上当了","大灰狼装绵羊","第一只小猪拜牛为师","第三只小猪办食物加工厂","三只小猪看望妈妈"
             };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +48,14 @@ public class EntertainActivity extends Activity {
                 new String[]{"image", "title"}, new int[]{R.id.images, R.id.titles});
         ListView mylist = (ListView) findViewById(R.id.mylist);
         mylist.setAdapter(simpleAdapter);
-         //为ListView的列表项的单击事件绑定事件监听器
-            mylist.setOnItemClickListener(new OnItemClickListener() {
-        @Override
+        //为ListView的列表项的单击事件绑定事件监听器
+        mylist.setOnItemClickListener(new OnItemClickListener() {
+            @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            //利用intent传递影片的路径
+                //利用intent传递影片的路径
                 Intent intent =new Intent();
                 String data;
-            //选择不同的position的情况
+                //选择不同的position的情况
                 switch(position){
                     case 0:
                         data="android.resource://" + getPackageName() + "/" +R.raw.p1;
@@ -70,6 +71,62 @@ public class EntertainActivity extends Activity {
                         intent.setClass(EntertainActivity.this, VideoplayActivity.class);
                         startActivity(intent);
                         break;
+
+                    case 2:
+                        data="android.resource://" + getPackageName() + "/" +R.raw.p3;
+                        intent.putExtra("vaddress", data);
+                        intent.setClass(EntertainActivity.this, VideoplayActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case 3:
+                        data="android.resource://" + getPackageName() + "/" +R.raw.p4;
+                        intent.putExtra("vaddress", data);
+                        intent.setClass(EntertainActivity.this, VideoplayActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case 4:
+                        data="android.resource://" + getPackageName() + "/" +R.raw.p5;
+                        intent.putExtra("vaddress", data);
+                        intent.setClass(EntertainActivity.this, VideoplayActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case 5:
+                        data="android.resource://" + getPackageName() + "/" +R.raw.p6;
+                        intent.putExtra("vaddress", data);
+                        intent.setClass(EntertainActivity.this, VideoplayActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case 6:
+                        data="android.resource://" + getPackageName() + "/" +R.raw.p7;
+                        intent.putExtra("vaddress", data);
+                        intent.setClass(EntertainActivity.this, VideoplayActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case 7:
+                        data="android.resource://" + getPackageName() + "/" +R.raw.p8;
+                        intent.putExtra("vaddress", data);
+                        intent.setClass(EntertainActivity.this, VideoplayActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case 8:
+                        data="android.resource://" + getPackageName() + "/" +R.raw.p9;
+                        intent.putExtra("vaddress", data);
+                        intent.setClass(EntertainActivity.this, VideoplayActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case 9:
+                        data="android.resource://" + getPackageName() + "/" +R.raw.p10;
+                        intent.putExtra("vaddress", data);
+                        intent.setClass(EntertainActivity.this, VideoplayActivity.class);
+                        startActivity(intent);
+                        break;
                 }
             }
         });
@@ -77,7 +134,7 @@ public class EntertainActivity extends Activity {
 
     }
 
-    }
+}
 
 
 
